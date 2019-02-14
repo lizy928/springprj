a1be67765b5a0632a892469c01689ec26163e69a
李正元 <lizhengyuan@11max.com>
2019-02-14 16:55:46 +0800

    add interceptor file

---
+++ src/main/java/com/lizy/springprj/interceptor/LoginInterceptor.java
@@ -0,0 +1,14 @@
+package com.lizy.springprj.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * Created By Lizhengyuan on 19-2-14
+ */
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+
+
+
+}
